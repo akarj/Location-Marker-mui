52a9ffd15eda4048b8ea15191063ffc0a7a81c60
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-10-15 23:38:52 +0530

    Register Axios lib  and form submission was added!!

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,23 +1,45 @@
 import "./Register.scss";
 import Button from "@mui/material/Button";
 import { Room } from "@mui/icons-material";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import axios from "axios";
 
 export default function Register() {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const newUser = {
+      username: usernameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    try {
+      await axios.post("/users/register", newUser);
+      setError(false);
+      setSuccess(true);
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
     <div className="register-container">
       <div className="logo">
         <Room /> Location Marker
       </div>
-      <form action="post">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="username"
           //  autoComplete="username"
           placeholder="username"
           id="username"
+          ref={usernameRef}
         />
         <input
           type="email"
@@ -25,13 +47,16 @@ export default function Register() {
           id="email"
           //  autoComplete="current-email"
           placeholder="email"
+          ref={emailRef}
         />
         <input
           type="password"
           name="password"
           id="password"
+          min="6"
           //  autoComplete="current-password"
           placeholder="password"
+          ref={passwordRef}
         />
         <Button
           type="submit"
